b7bca32353be793326f218e168b9af0e86350970
solorsystem1 <jingue0923@naver.com>
2018-05-09 23:26:37 +0900

    Parabolic implementation with angle and force input

--- source/hello.c
+++ source/hello.c
@@ -1,51 +1,78 @@
 #include <stdio.h>
 #include <curses.h>
 #include <unistd.h>
+#include <math.h>
+#define PI 3.1415926535
+#define Gravity 10
 
-int main()
+struct coeffient{
+    float angle;
+    float power;
+    float a;
+    float b;
+};
+
+typedef struct coeffient element;
+
+void equation(element *store){
+    store->b=-store->power;
+    store->a=tan(store->angle*PI/180.0)/2/store->b;
+}
+
+
+void parabola(element store,int pos_X,int pos_Y,char bomb){
+    int posX=pos_X,posY=pos_Y,i,temp;
+    
+    for(i=store.b;i<=-store.b;i++){
+        
+        temp=store.a*(i-store.b)*(i+store.b);
+        //temp*=0.3;
+        posX++;
+        posY=pos_Y-temp;
+        //addch(' ');
+        move(posY, posX);
+        addch(bomb);
+        move(posY, posX);
+        refresh();
+        usleep(30000-store.power*500+10000*sqrt(temp));
+    }
+}
+
+int main(void)
 {
-    int i;
     char bomb = '*';
-    char blank= ' ';
-    int arr[31][2];
+    
     int posX, posY;
-    float temp = 0;
+    element store;
+    
     initscr();
     clear();
-    posX = COLS-70;
+    posX = 5;
     posY = LINES-5;
-    for(i=0;i<30;i++)
-    {
-        move(posY,posX);
-        addch(bomb);
-        move(LINES - 1, COLS -1);
-        refresh();
-        usleep(500000);
-        move(posY,posX);
-        addch(blank);
-        
-        temp = 0-(((float)i/2)-7.5)*(((float)i/2)-7.5);
-        temp *= 0.3;
-        posX=COLS-70+i;
-        posY=LINES-5+(int)temp;
-        arr[i][0] = posX;
-        arr[i][1] = posY;
-    }
-    endwin();
-//    for(i=1;i<30;i++)
-//    {
-//        temp = 0-(((float)i/2)-7.5)*(((float)i/2)-7.5);
-//        temp *= 0.8;
-//        printf("%d ",(int)temp);
-//        posX+=i;
-//        posY+=(int)temp;
-//        printf("%d %d\n",posX,posY);
-//
-//    }
-//    for(i=0;i<30;i++)
-//    {
-//        printf("%d %d\n",arr[i][0],arr[i][1]);
-//    }
+    
+    /*for(i=0;i<30;i++)
+     {
+     move(posY,posX);
+     addch(bomb);
+     move(LINES - 1, COLS -1);
+     refresh();
+     usleep(300000);
+     move(posY,posX);
+     addch(blank);
+     
+     temp = 53-(((float)i/2)-7.5)*(((float)i/2)-7.5);
+     temp *= 0.3;
+     posX=COLS-70+i;
+     posY=LINES-5-(int)temp;
+     }*/
+    
+    scanf("%f %f",&store.angle,&store.power);
+    equation(&store);
+    
+    move(posY, posX);
+    
+    parabola(store, posX, posY, bomb);
     
     return 0;
 }
+
