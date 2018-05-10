d6d3e6c34f65d5c3513b6fcade5dff79b5a98f0c
solorsystem1 <jingue0923@naver.com>
2018-05-10 20:18:36 +0900

    modifiy 'power' element,add 'direction' element

--- source/parabola.c
+++ source/parabola.c
@@ -1,45 +1,70 @@
 #include <stdio.h>
 #include <curses.h>
 #include <unistd.h>
+#include <termios.h>
 #include <math.h>
 #define PI 3.1415926535
-#define Gravity 10
+#define Gravity 9.8
 
 struct coeffient{
     float angle;
     float power;
     float a;
     float b;
+    int direction;
 };
 
 typedef struct coeffient element;
 
 void equation(element *store){
-    store->b=-store->power;
+    store->b=-pow(store->power,2)*cos(store->angle*PI/180.0)*sin(store->angle*PI/180.0)/Gravity;
     store->a=tan(store->angle*PI/180.0)/2/store->b;
 }
 
 
 void parabola(element store,int pos_X,int pos_Y,char bomb){
     int posX=pos_X,posY=pos_Y,i,temp;
-    int lateX, lateY;
-    for(i=store.b;i<=-store.b;i++){
-        
-        temp=store.a*(i-store.b)*(i+store.b);
-        //temp*=0.3;
-        posX++;
-        posY=pos_Y-temp;
-        //addch(' ');
-        move(posY, posX);
-        addch(bomb);
-        //move(posY, posX);
-        move(LINES-1,COLS-1);
-        refresh();
-        move(posY, posX);
-        addch(' ');
-        move(posY, posX);
-        usleep(30000-store.power*500+10000*sqrt(temp));
+    
+    if(store.direction==0){
+        for(i=store.b;i<=-store.b;i++){
+            
+            temp=store.a*(i-store.b)*(i+store.b);
+            //temp*=0.7;
+            posX++;
+            if(posX<=0||posX>=COLS)
+                break;
+            posY=pos_Y-temp;
+            if(posY<=0||posY>=LINES)
+                break;
+            //addch(' ');
+            move(posY, posX);
+            addch(bomb);
+            move(posY, posX);
+            refresh();
+            usleep(40000-store.power*500);
+        }
     }
+    else{
+        for(i=-store.b;i>=store.b;i--){
+            
+            temp=store.a*(i-store.b)*(i+store.b);
+            //temp*=0.7;
+            posX--;
+            if(posX<=0||posX>=COLS)
+                break;
+            posY=pos_Y-temp;
+            if(posY<=0||posY>=LINES)
+                break;
+            //addch(' ');
+            move(posY, posX);
+            addch(bomb);
+            move(posY, posX);
+            refresh();
+            usleep(40000-store.power*500);
+        }
+        
+    }
+    sleep(1);
 }
 
 int main(void)
@@ -49,35 +74,34 @@ int main(void)
     int posX, posY;
     element store;
     
-    printf("Input angle and power : ");
-    scanf("%f %f",&store.angle,&store.power);
+    printf("input angle power direction(Left:1,Right:0) : ");
+    scanf("%f %f %d",&store.angle,&store.power,&store.direction);
+    
     initscr();
     clear();
-    posX = 5;
+    if(store.direction==0)
+        posX = 5;
+    else
+        posX = COLS-5;
     posY = LINES-5;
     
-    /*for(i=0;i<30;i++)
-     {
-     move(posY,posX);
-     addch(bomb);
-     move(LINES - 1, COLS -1);
-     refresh();
-     usleep(300000);
-     move(posY,posX);
-     addch(blank);
-     
-     temp = 53-(((float)i/2)-7.5)*(((float)i/2)-7.5);
-     temp *= 0.3;
-     posX=COLS-70+i;
-     posY=LINES-5-(int)temp;
-     }*/
+    
+    if(store.power>60)
+        store.power=60;
     
     equation(&store);
     
     move(posY, posX);
     
     parabola(store, posX, posY, bomb);
+    
     endwin();
+    
+    printf("coeffient a:%f coeffient b:%f angle:%f power:%f ",store.a,store.b,store.angle,store.power);
+    if(store.direction==1)
+        printf("direction:Left \n");
+    else
+        printf("direction:Right \n");
     return 0;
 }
 
